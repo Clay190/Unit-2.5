a7763ea20e0257e1dee62bf8a21dbca582632363
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-22 14:30:03 -0400

    Updated from Brython Server: 9/22/2017 2:30:02 PM

--- house.py
+++ house.py
@@ -4,6 +4,15 @@
 
 from ggame import *
 
-brown = (0x8B4513,1)
+brown = Color(0x8B540F,1)
+blueLight = Color(30-144-255)
 
 
+brownOutline = LineStyle(2,brown)
+
+brownTriangle = PolygonAsset([(250,0),(0,200),(500,200)],brownOutline,brown)
+blueWindow = RectangleAsset
+
+Sprite(brownTriangle)
+Sprite(blueWindow)
+App().run()
