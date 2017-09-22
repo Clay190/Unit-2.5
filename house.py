007f8f8d0761d5d8d8420645eb102bddfabfa4b5
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-22 14:48:03 -0400

    Updated from Brython Server: 9/22/2017 2:48:02 PM

--- house.py
+++ house.py
@@ -5,7 +5,7 @@
 from ggame import *
 
 brown = Color(0x8B540F,1)
-blueLight = Color(0x1E90FF,0.7)
+blueLight = Color(0x0080FF,1)
 black = Color(0x000000,1)
 darkBrown = Color(0x644628,1)
 pink = Color(0xAA3FAE,1)
@@ -19,6 +19,6 @@ wall = RectangleAsset(400,300,blackOutline,darkBrown)
 
 Sprite(wall, (50,200))
 Sprite(brownTriangle)
-Sprite(pinkDoor,(120,290))
+Sprite(pinkDoor,(120,350))
 Sprite(blueWindow,(300,300))
 App().run()
