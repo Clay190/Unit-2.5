a798cc3f28aa8aaf44c5e64a695d7e157b9c734c
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-26 14:41:32 -0400

    Updated from Brython Server: 9/26/2017 2:41:32 PM

--- name.py
+++ name.py
@@ -1,1 +1,20 @@
+#Clay Kynor
+#9/26/17
+#name.py
 
+from ggame import *
+
+name=input("Please enter your name")
+code=input("Enter an RGB color code with a 0x in front of it!")
+
+color = Color(code,1)
+black = Color(0x000000,1)
+
+colorOutline = LineStyle(1,color)
+
+text = TextAsset(name, fill=black,style="bold 20pt Times")
+backround = RectangleAsset(3000,2000,colorOutline,color)
+
+Sprite(backround)
+Sprite(text,(450,250))
+App().run()
