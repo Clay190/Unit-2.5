d986e8186f471b9684d12c0f39275a5bee97b459
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-26 14:54:05 -0400

    Updated from Brython Server: 9/26/2017 2:54:04 PM

--- olympics.py
+++ olympics.py
@@ -1,1 +1,25 @@
+#Clay Kynor
+#9/26/17
+#olympics.py 
+
+from ggame import *
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+yellow = Color(0xFFFF00,1)
+white = Color(0xFFFFFF,1)
+
+redOutline = LineStyle(5,red)
+greenOutline = LineStyle(5,green)
+blueOutline = LineStyle(5,blue)
+blackOutline = LineStyle(5,black)
+yellowOutline = LineStyle(5,yellow)
+
+redCircle = CircleAsset(100,redOutline,white)
+greenCircle = CircleAsset(100,greenOutline,white)
+blueCircle = CircleAsset(100,blueOutline,white)
+blackCircle = CircleAsset(100,blackOutline,white)
+yellowCircle = CircleAsset(100,yellowOutline,white)
 
